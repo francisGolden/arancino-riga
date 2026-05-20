d099a0758b64f16cede2ffd9c764380f1beb8a43
Francesco Doria <doria5@live.it>
2026-05-20 10:59:46 +0300

    remove tailwind import

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { devtools } from '@tanstack/devtools-vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
