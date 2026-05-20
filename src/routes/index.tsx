9ecc22f25dc864111b08eff7a577384932b7a275
Francesco Doria <doria5@live.it>
2026-05-20 16:37:05 +0300

    show passing time in UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,21 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { getElapsedGameTime } from '#/engine/world/time'
+import { useState } from 'react'
 
 export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
-  const {elapsedSeconds} = getElapsedGameTime()
+  const [elapsed, setElapsed] = useState(0)
+  setInterval(() => {
+    const { elapsedTime } = getElapsedGameTime()
+
+    setElapsed(elapsedTime)
+  }, 5000)
 
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <span>gameStartedAt: {elapsedSeconds/60}</span>
+      <span>gameStartedAt: {(Math.floor(elapsed / 1000))/60}</span>
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
