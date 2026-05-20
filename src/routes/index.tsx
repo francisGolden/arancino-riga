cd9e5c7152db836b4abc3050d704da7c81cc70e3
Francesco Doria <doria5@live.it>
2026-05-20 16:46:39 +0300

    elapsed mins

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,16 +6,18 @@ export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   const [elapsed, setElapsed] = useState(0)
+
   setInterval(() => {
     const { elapsedTime } = getElapsedGameTime()
-
     setElapsed(elapsedTime)
-  }, 5000)
+  }, 2000)
+
+  const elapsedMinutes = (Math.floor(elapsed / 1000))/60
 
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-      <span>gameStartedAt: {(Math.floor(elapsed / 1000))/60}</span>
+      <span>elapsed minutes: {elapsedMinutes}</span>
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
