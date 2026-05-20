9a3661d1a8005ca4a17d1de28de5d3e44c633504
Francesco Doria <doria5@live.it>
2026-05-20 11:32:59 +0300

    start adding tick calc

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { currentTime } from '#/engine/world/time'
 
 export const Route = createFileRoute('/')({ component: Home })
 
@@ -6,6 +7,7 @@ function Home() {
   return (
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
+      <span>current time: {currentTime}</span>
       <p className="mt-4 text-lg">
         Edit <code>src/routes/index.tsx</code> to get started.
       </p>
