79ff6e38832bfb99543eddcb0c7cef13228571af
Francesco Doria <doria5@live.it>
2026-05-28 16:03:44 +0300

    offlineProgressStatus states instead of offlineProgressProcessed boolean

--- src/store/loop.ts
+++ src/store/loop.ts
@@ -2,45 +2,64 @@ import { create } from 'zustand'
 
 interface LoopState {
   offlineDelta: number
-  offlineProgressProcessed: boolean
+  offlineProgressStatus: 'idle' | 'processing' | 'done'
   processOfflineProgress: (offlineDelta: number) => Promise<boolean>
   setOfflineDelta: (lastSavedAt: number, currentTime: number) => number
 }
 
 export const useLoop = create<LoopState>((set, get) => ({
   offlineDelta: 0,
-  offlineProgressProcessed: false,
+  offlineProgressStatus: 'idle',
   processOfflineProgress: async (offlineDelta: number): Promise<boolean> => {
-    const offlineProgressProcessed = get().offlineProgressProcessed
-    if (!offlineProgressProcessed) {
-      set(() => ({ offlineProgressProcessed: true }))
-
-      // offline processing simulation with promises
-      const promises = [
-        new Promise((resolve) => {
-          setTimeout(() => resolve('action 1 completed with offline delta ' + offlineDelta), 2000)
-        }),
-        new Promise((resolve) => {
-          setTimeout(() => resolve('action 2 completed with offline delta ' + offlineDelta), 3000)
-        }),
-        new Promise((resolve) => {
-          setTimeout(() => resolve('action 3 completedwith offline delta ' + offlineDelta), 4000)
-        }),
-      ]
-
-      try {
-        console.log('start processing')
-        const results = await Promise.allSettled(promises)
-        console.log('All completed!', results)
-        return true
-      } catch (error) {
-        console.error('Something went wrong :(', error)
-        set(() => ({ offlineProgressProcessed: false }))
-        return false
-      }
+    const offlineProgressStatus = get().offlineProgressStatus
+    if (offlineProgressStatus === 'processing') {
+      console.log('still processing...')
+      return false
     }
-    console.log('Processing already working...')
-    return false
+    if (offlineProgressStatus === 'done') {
+      console.log('already processed offline progress')
+      return false
+    }
+
+    set(() => ({ offlineProgressStatus: 'processing' }))
+
+    // offline processing simulation with promises
+    const promises = [
+      new Promise((resolve) => {
+        setTimeout(
+          () =>
+            resolve('action 1 completed with offline delta ' + offlineDelta),
+          2000,
+        )
+      }),
+      new Promise((resolve) => {
+        setTimeout(
+          () =>
+            resolve('action 2 completed with offline delta ' + offlineDelta),
+          3000,
+        )
+      }),
+      new Promise((resolve) => {
+        setTimeout(
+          () => resolve('action 3 completedwith offline delta ' + offlineDelta),
+          4000,
+        )
+      }),
+    ]
+
+    try {
+      console.log('start processing')
+      const results = await Promise.allSettled(promises)
+      set(() => ({ offlineProgressStatus: 'done' }))
+      console.log('All completed!', results)
+      return true
+    } catch (error) {
+      console.error('Something went wrong :(', error)
+      set(() => ({
+        offlineProgressStatus: 'idle',
+      }))
+      return false
+    }
   },
   setOfflineDelta: (lastSavedAt: number, currentTime: number) => {
     set(() => ({ offlineDelta: currentTime - lastSavedAt }))
